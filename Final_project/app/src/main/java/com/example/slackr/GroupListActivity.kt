44532e272f55f2a2a6e33cb06d39bd60b9d5c6a3
Sonso <32463988+sonso@users.noreply.github.com>
2020-12-02 23:58:13 -0500

    More Matching

---
+++ Final_project/app/src/main/java/com/example/slackr/GroupListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.slackr
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GroupListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match)
+
+    }
+}
