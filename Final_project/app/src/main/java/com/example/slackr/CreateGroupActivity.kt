f55dc55a9e7615be0d70c3d5bf9e17c5a5a71e26
Sam Soon <ssoon@umd.edu>
2020-12-03 19:40:09 -0500

    sonny

---
+++ Final_project/app/src/main/java/com/example/slackr/CreateGroupActivity.kt
@@ -0,0 +1,13 @@
+package com.example.slackr
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match)
+
+    }
+}
